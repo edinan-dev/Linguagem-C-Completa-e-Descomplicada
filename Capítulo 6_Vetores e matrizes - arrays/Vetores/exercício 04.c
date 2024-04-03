3f91d867d7f3cb9fa06566f6045749d85c91f475
edinan-dev <98602571+edinan-dev@users.noreply.github.com>
2024-04-02 21:36:25 -0300

    001

---
+++ Capítulo 6_Vetores e matrizes - arrays/Vetores/exercício 04.c
@@ -0,0 +1,30 @@
+/*Faça um programa que possua um array de nome A que armazene seis números inteiros.
+O programa de executar os seguintes passos:
+a) Atribua os seguintes valores a esse array: 1, 0, 5, -2, -5, 7.
+b) Armazene em uma variável a soma dos valores das posições A[0], A[1] e A[5] do array 
+e mostre na tela essa soma.
+c) Modifique o array na posiçãp 4, atribuindo a essa posição o valor 100.
+d) Mostre na tela cada valor do array, um em cada linha.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int A[6] = {1, 0, 5, -2, -5, 7};
+    int soma, contador;
+
+    soma = A[0] + A[1] + A[5];
+    printf("A soma de %d, %d e %d\n",A[0], A[1], A[5]);
+
+    A[4] = 100;
+
+    printf("Os numeros no vetor sao:");
+    for(contador = 0; contador < 6; contador++) {
+        printf("%d\n",A[contador]);
+    }
+
+
+    system("pause");
+    return 0;
+}
