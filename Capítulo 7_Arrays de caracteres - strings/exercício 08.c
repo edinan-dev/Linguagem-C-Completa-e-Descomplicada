2238994d5190a68a78d1211a50f22ed4cb256ae2
edinan-dev <98602571+edinan-dev@users.noreply.github.com>
2024-06-25 23:18:17 -0300

    tem um erro na logica mas funciona

--- Capítulo 7_Arrays de caracteres - strings/exercício 08.c
+++ Capítulo 7_Arrays de caracteres - strings/exercício 08.c
@@ -7,8 +7,40 @@ mensagem informando se a segunda string está contida dentro da primeira.*/
 
 int main(){
 
-    
+    char str_1[20], str_2[20];
+    int i = 0, j = 0;
+    int encontrado = 0;
 
+    //Ler string 01//
+    printf("Digite um palavra: ");
+    fgets(str_1, 20, stdin);
+    str_1[strcspn(str_1, "\n")] = '\0';
+
+    //Ler string 02//
+    printf("Digite um palavra: ");
+    fgets(str_2, 20, stdin);
+    str_1[strcspn(str_2, "\n")] = '\0';
+
+    while (str_1[i] != '\0') {
+        while (str_1[j] != '\0' && str_1[i + j] == str_2[j]) {
+            j++;
+        }
+        
+        if (str_2[j] == '\0') {
+            encontrado = 1;
+            break;
+        }
+        
+        j = 0;
+        i++;
+    }
+
+    if(encontrado == 0) {
+        printf("String encontrada\n");
+    }
+    else {
+        printf("String nao encontrada\n");
+    }
     system("pause");
     return 0;
 } 
